e54393a80c995ec6bf6640a273784b6087056ef6
shash-2106 <raoshashwati@gmail.com>
2025-08-28 23:22:18 +0530

    logo size changed

--- src/components/common/LandingHeader.jsx
+++ src/components/common/LandingHeader.jsx
@@ -1,6 +1,7 @@
 
 "use client";
 import React from 'react';
+import Image from 'next/image';
 import './LandingHeader.css';
 
 function LandingHeader() {
@@ -8,8 +9,16 @@ function LandingHeader() {
     <header className="landing-header">
       <div className="logo-container">
         <div className="logos-wrapper">
-        <img src="/assets/cc_white_logo.png" alt="CC Logo" className="header-logo" />
-        <img src="/assets/rvce_white_logo.png" alt="RVCE Logo" className="header-logo" />      </div>
+        <Image src="/assets/rvce_white_logo.png" alt="RVCE Logo" width={250}  
+          height={80}/> 
+        <Image
+          src="/assets/cc_white_logo.png"
+          alt="CC Logo"
+          width={125}  
+          height={40} 
+        />
+             
+          </div>
         <div className="text-content">
           <h1 className="logo-text">This or That</h1>
           <p className="tagline">The real-time classroom debate game.</p>
